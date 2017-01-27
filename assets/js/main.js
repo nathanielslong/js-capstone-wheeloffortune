e8d67739f3201b07f3e2f19d32fa9c3c8aba4127
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-01-27 14:25:51 -0500

    got gameboard display to work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,13 @@
-// Alright, here's what I need to do for this project.
-//
 // Develop a wheel.
 // Develop Hangman game.
 // Integrate the two.
-//
-// Should be easy, right?
 // 
 // First, let's do Hangman.
 //
 // Let's figure the components of Hangman. 
 // We need:
 // - a database of categories and words/phrases in the categories
+// - when user loads page, allow user to pick category then select random puzzle
 // - a way to display the blank version of the word/phrase
 // - something that allows the user to guess
 // - something that determines if the answer is correct
@@ -37,7 +34,7 @@ function Category(title) {
 }
 
 // Adding puzzles and pushing to categories.
-var goneWithWind= new Puzzle("Frankly my dear, I don't give a damn");
+var goneWithWind= new Puzzle("frankly, my dear, i don't give a damn");
 var wizardOz= new Puzzle("Toto, I've a feeling we're not in Kansas anymore");
 var citizenKane = new Puzzle("Rosebud");
 var starWars = new Puzzle("May the Force be with you");
@@ -55,8 +52,48 @@ philosophers.puzzles.push(confuscious, immKant, davHume, petSing, aristotle);
 
 var hydrogen = new Puzzle("Hydrogen");
 var indium = new Puzzle("Indium");
-var polSty= new Puzzle("Polystyrene");
+var polSty = new Puzzle("Polystyrene");
 var erlen = new Puzzle("Erlenmeyer");
 var spectro = new Puzzle("Spectroscopy");
 var chemistry = new Category("Chemistry-related questions");
 chemistry.puzzles.push(hydrogen, indium, polSty, erlen, spectro);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+function makeBoard(phrase) {
+  phrase = phrase.replace(/[a-zA-Z]/g, "?");
+  var phraseArray = phrase.split("");
+  var gameBoard = "";
+  for (i=0;i<phraseArray.length;i++) {
+    gameBoard += "<div class=" + (phraseArray[i] == " " ? "space" : "letter") +" id='letter-" + i + "'></div>";
+  }
+  $("#gameboard").html(gameBoard);
+}
+
+function makeGuess(letter) {
+  
+}
+
+$("#movieQuotes").click(function() {
+  var selectPhrase = movieQuotes.puzzles[getRandomInt(0,movieQuotes.puzzles.length-1)].phrase
+  $(".initial-page").fadeOut();
+  makeBoard(selectPhrase);
+})
+
+$("#philosophers").click(function() {
+  var selectPhrase = philosophers.puzzles[getRandomInt(0,philosophers.puzzles.length-1)].phrase
+  $(".initial-page").fadeOut();
+  makeBoard(selectPhrase);
+})
+
+$("#chemistry").click(function() {
+  var selectPhrase = chemistry.puzzles[getRandomInt(0,chemistry.puzzles.length-1)].phrase
+  $(".initial-page").fadeOut();
+  makeBoard(selectPhrase);
+})
+
+
