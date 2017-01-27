c0a11bd1b40a74fd1cf05190ca0b08a061aca996
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-01-27 17:43:17 -0500

    actual commit, was in wrong folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,11 @@
 // Develop a wheel.
 // Develop Hangman game.
 // Integrate the two.
-// 
-// First, let's do Hangman.
 //
 // Let's figure the components of Hangman. 
 // We need:
-// - a database of categories and words/phrases in the categories
-// - when user loads page, allow user to pick category then select random puzzle
-// - a way to display the blank version of the word/phrase
-// - something that allows the user to guess
-// - something that determines if the answer is correct
-//    - if answer correct, output to the blank spaces
-//    - if not, something happens to make the user fail
 // - if the user gets the whole thing right, something to congratulate
 // - if user getse enough wrong, something to scold them and make them try again
-//
-// I think that's everything. So let's just do things.
-//
-// First, the database of categories and words/phrases.
-// Let's make them objects, that seems like a smart idea. Maybe we could let the user eventually add their own things?
-// We'll make the puzzles (words and phrases) and put them inside of a category object.
 
 // Defining the Puzzle object.
 function Puzzle(phrase) {
@@ -58,42 +43,97 @@ var spectro = new Puzzle("Spectroscopy");
 var chemistry = new Category("Chemistry-related questions");
 chemistry.puzzles.push(hydrogen, indium, polSty, erlen, spectro);
 
+// Gives a random number in a certain range, which we need to give a random selection from a certain category.
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random()*(max-min+1))+min;
 }
 
+// Generates a new puzzle board given the randomly selected phrase.
 function makeBoard(phrase) {
-  phrase = phrase.replace(/[a-zA-Z]/g, "?");
-  var phraseArray = phrase.split("");
+  var newPhrase = phrase.replace(/[a-zA-Z]/g, "?");
+  var phraseArray = newPhrase.split("");
   var gameBoard = "";
   for (i=0;i<phraseArray.length;i++) {
-    gameBoard += "<div class=" + (phraseArray[i] == " " ? "space" : "letter") +" id='letter-" + i + "'></div>";
+    var addedClass;
+    if (phraseArray[i] == " ") {
+      addedClass = "space";
+    } else if (phraseArray[i] == "?") {
+      addedClass = "letter";
+    } else {
+      addedClass = "not-letter";
+    }
+    gameBoard += "<div class=" + addedClass +" id='letter-" + i + "'></div>";
   }
   $("#gameboard").html(gameBoard);
+  var notLetIndex = [];
+  for (i=0;i<phraseArray.length;i++) {
+    if (/[a-z]/i.test(phraseArray[i]) == false) {
+      notLetIndex.push(i);
+    }
+  } 
+  for (i=0;i<notLetIndex.length;i++) {
+    $("#letter-" + notLetIndex[i]).html(phraseArray[notLetIndex[i]]);
+  }
 }
 
+// Takes a user inputted guess and determines if it exists in the array. Still needs a way to tell if the puzzle is completed. Also needs something to track what guesses have been done.
 function makeGuess(letter) {
-  
+  var guess = $("input").val();
+  var answerArray = [];
+  var phraseArray = selectPhrase.split("");
+  for (i=0;i<phraseArray.length;i++) {
+    if (guess == phraseArray[i].toLowerCase()) {
+      answerArray.push(i);
+    }
+  }
+  if (answerArray.length == 0) {
+    $("#correctness").text("Guess Again!");
+    $("#letter-guessed").append(guess + " ");
+  } else {
+    $("#correctness").text("Correct!");
+    for (i=0;i<answerArray.length;i++) {
+      var letter = phraseArray[answerArray[i]];
+      phraseArray.splice[answerArray[i]];
+      $("#letter-" + answerArray[i]).html(letter);
+      $("#letter-" + answerArray[i]).css({"background-color": "white"})
+    }
+  }
 }
 
+// Sets the movieQuote object so that when clicked, it gets a random puzzle and sets up the board. 
 $("#movieQuotes").click(function() {
-  var selectPhrase = movieQuotes.puzzles[getRandomInt(0,movieQuotes.puzzles.length-1)].phrase
+  randNum = getRandomInt(0, movieQuotes.puzzles.length - 1);
+  selectPhrase = movieQuotes.puzzles[randNum].phrase
   $(".initial-page").fadeOut();
+  $("form").fadeIn();
+  $(".guesses").fadeIn();
   makeBoard(selectPhrase);
 })
 
+// Sets the philosophers object so that when clicked, it gets a random puzzle and sets up the board. 
 $("#philosophers").click(function() {
-  var selectPhrase = philosophers.puzzles[getRandomInt(0,philosophers.puzzles.length-1)].phrase
+  randNum = getRandomInt(0, philosophers.puzzles.length - 1);
+  selectPhrase = philosophers.puzzles[randNum].phrase
   $(".initial-page").fadeOut();
+  $("form").fadeIn();
+  $(".guesses").fadeIn();
   makeBoard(selectPhrase);
 })
 
+// Sets the chemistry object so that when clicked, it gets a random puzzle and sets up the board. 
 $("#chemistry").click(function() {
-  var selectPhrase = chemistry.puzzles[getRandomInt(0,chemistry.puzzles.length-1)].phrase
+  randNum = getRandomInt(0, chemistry.puzzles.length - 1);
+  selectPhrase = chemistry.puzzles[randNum].phrase
   $(".initial-page").fadeOut();
+  $("form").fadeIn();
+  $(".guesses").fadeIn();
   makeBoard(selectPhrase);
 })
 
-
+// Prevents the form object from doing its default page-switching nonsense and starts the guess function.
+$("form").submit(function (e) {
+  e.preventDefault();
+  makeGuess($("input").val());
+})
